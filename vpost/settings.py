29a9529c2a9634e2e729dae9a9c31dafa1a3861f
Mehedi Hasan <43444174+mehedihasan555552@users.noreply.github.com>
2024-11-13 02:33:57 +0600

    Update settings.py

--- vpost/settings.py
+++ vpost/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'iiiufusfjksjdfhjj76786zer67e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
